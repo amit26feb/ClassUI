5f599590aa4c35ad7ce1da597f546904bfc36751
Amit Kumar <amit.pisces.88@gmail.com>
2021-01-06 13:43:43 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,82 +1,99 @@
 import './App.css';
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-
+import {Container, Row, Col} from 'reactstrap';
 
 
 function App() {
-  var [selectedClass, textAreaData] = React.useState("");
+    var [selectedClass, setSelectedClass] = React.useState("");
+    var [textAreaData, setTextArea] = React.useState("");
+    var [apiResponse, setApiResponse] = React.useState("");
 
-  function handleChange(event) {
-    textAreaData = event.target.value;
-    console.log(textAreaData);
-  }
+    function handleChange(event) {
+        setTextArea(event.target.value);
+        // console.log(textAreaData);
+    }
 
-  function _onSelect(event) {
-    selectedClass = event.target.value;
-    console.log(selectedClass);
-  }
+    function _onSelect(event) {
+        setSelectedClass(event.target.value);
+        // console.log(selectedClass);
+    }
 
-  function handleSubmit() {
-    console.log(selectedClass, textAreaData);
-    let classInfo = { className: selectedClass, attendenceData: textAreaData };
-    fetch("http://localhost:58882/GenerateExcel", {
-      method: 'POST',
-     
-      body: JSON.stringify(classInfo),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        'accept':'*/*'
-      },
-      credentials: 'same-origin'
-     
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-      })
-  }
+    function handleSubmit() { // console.log(selectedClass, textAreaData);
+        let classInfo = {
+            className: selectedClass,
+            attendenceData: textAreaData
+        };
+        fetch("https://geetaclassapi.herokuapp.com/GenerateExcel", {
+            method: 'POST',
+
+            body: JSON.stringify(classInfo),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                'accept': '*/*'
+            },
+            credentials: 'same-origin'
+
+        }).then(response => response.json()).then(data => {
+            console.log(data)
+            setApiResponse(data);
+            console.log(apiResponse)
+        }).catch(error => {
+            setApiResponse(apiResponse);
+        })
+    }
 
 
-  return (
-    <div>
-      <Container>
-        <Row><Col xs="auto"><h3> Class</h3></Col>
-        </Row>
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col xs="auto">
+                        <h3>
+                            Class</h3>
+                    </Col>
+                </Row>
 
-        <form className="form-group" >
-          <Row >
-            <Col xs="5">
-              <select className="form-control" onChange={_onSelect} >
-                <option id="Select Class" value="NA">Select Class</option>
-                <option id="6" value="6">6th</option>
-                <option id="7" value="7">7th</option>
-                <option id="8" value="8">8th</option>
-                <option id="9" value="9">9th</option>
-                <option id="10" value="10">10th</option>
-              </select>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="3">
-              <input className="btn btn-success" type="button" value="Submit" onClick={handleSubmit} />
-            </Col>
-            <Col xs="3">
-              <input className="btn btn-danger" type="reset" value="Clear" />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="5">
-              <textarea className="form-control"
-                name="textValue" rows={10} cols={10}
-                onChange={handleChange} placeholder="Paste data here"
-              />
-            </Col>
-          </Row>
-        </form>
-      </Container>
-    </div>
-  );
+                <form className="form-group">
+                    <Row>
+                        <Col xs="5">
+                            <select className="form-control"
+                                onChange={_onSelect}>
+                                <option id="Select Class" value="NA">Select Class</option>
+                                <option id="6" value="6">6th</option>
+                                <option id="7" value="7">7th</option>
+                                <option id="8" value="8">8th</option>
+                                <option id="9" value="9">9th</option>
+                                <option id="10" value="10">10th</option>
+                            </select>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs="3">
+                            <input className="btn btn-success" type="button" value="Submit"
+                                onClick={handleSubmit}/>
+                        </Col>
+                        <Col xs="3">
+                            <input className="btn btn-danger" type="reset" value="Clear"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p>{apiResponse}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs="5">
+                            <textarea className="form-control" name="textValue"
+                                rows={10}
+                                cols={10}
+                                onChange={handleChange}
+                                placeholder="Paste data here"/>
+                        </Col>
+                    </Row>
+                </form>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
